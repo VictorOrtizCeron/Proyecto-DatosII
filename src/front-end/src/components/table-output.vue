3a05a93519af30f643c06f79b860956775b9571c
Victor Ortiz <victorandresortiz@gmail.com>
2023-06-09 15:58:50 -0600

    creación de popup para creación de ventana y envió de info a backend implementada

--- src/front-end/src/components/table-output.vue
+++ src/front-end/src/components/table-output.vue
@@ -10,6 +10,37 @@
     <button type="button" class="btn btn-outline-success" @click="sendQuery">Query</button>
     <button type="button" class="btn btn-outline-danger">Commit</button>
 
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+      Crear Tabla
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Información de la tabla</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+
+            <input type="text" class="form-control popup" v-model="newTableName" placeholder="Nombre de la tabla">
+
+
+            <textarea class="form-control popup" aria-label="With textarea" placeholder="Columnas de la tabla"
+                      v-model="newTableColumns"> </textarea>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-primary" @click="sendColumnData">Crear Tabla</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 
   <div class="container-fluid">
@@ -60,6 +91,8 @@ export default {
 
       ],
       query: '',
+      newTableName: '',
+      newTableColumns: ''
     }
   },
   methods: {
@@ -75,6 +108,18 @@ export default {
         // Handle the error if needed
       });
     },
+    sendColumnData() {
+      axios.post("/api/database/create", {
+        newTableName: this.newTableName,
+        newTableColumns: this.newTableColumns
+      })
+          .then((response) => {
+            alert(response.data);
+          }
+      ).catch((error) => {
+        console.error("Error sending text:", error);
+      })
+    }
   },
 
 }
@@ -85,13 +130,18 @@ export default {
 
   padding: 50px;
 }
-.btn-outline-success, .btn-outline-danger {
+
+.btn-outline-success, .btn-outline-danger, .btn-outline-primary {
   width: 10%;
   margin: 5px;
 }
 
 .form-control {
   font-size: 1.5rem;
+}
+
+.popup {
+  margin-top: 10px;
 }
 
 textarea {
--- src/main/java/com/example/mydatabasece/controller/queryController.java
+++ src/main/java/com/example/mydatabasece/controller/queryController.java
@@ -8,26 +8,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
+
 //controlador de queries , aqui llegan las solicitudes de la página principal mediante el método post
 @RestController
 @RequestMapping("/api/database")
 public class queryController {
-            @PostMapping("/table")
-            ResponseEntity<String> queryPost(@RequestBody queryRequest request) {
+    @PostMapping("/table")
+    ResponseEntity<String> queryPost(@RequestBody queryRequest request) {
 
-                System.out.println(request.getQuery());
+        System.out.println(request.getQuery());
 
 
-                return ResponseEntity.ok("Query received in backend");
+        return ResponseEntity.ok("Query received in backend");
 
-            }
+    }
+    //request de query básico
+    public static class queryRequest {
+        private String query;
 
-            public static class queryRequest {
-                private String query;
+        public String getQuery() {
+            return query;
+        }
 
-                public String getQuery() {
-                    return query;
-                }
+
+    }
+    @PostMapping("/create")
+    ResponseEntity<String> queryPost(@RequestBody newTableRequest request) {
+
+        System.out.println(request.getNewTableName());
+
+        System.out.println(request.getNewTableColumns());
+
+        return ResponseEntity.ok("New Table creation request received in backend");
+
+    }
+    //request de creación de nueva base de datos
+    public static class newTableRequest {
+        private String newTableName;
+
+        private String newTableColumns;
+
+        public String getNewTableName() {
+            return newTableName;
+        }
+        public String getNewTableColumns() {
+            return newTableColumns;
+        }
 
 
     }
