33016095e828684299314840ac4c7a8a0e1aced5
Victor Ortiz <victorandresortiz@gmail.com>
2023-06-11 09:02:56 -0600

    agregación de funcionalidad de XML Stores

--- src/front-end/src/components/sidebar.vue
+++ src/front-end/src/components/sidebar.vue
@@ -8,30 +8,53 @@
         MyDatabaseCE
 
       </a>
-      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions"
-              aria-controls="offcanvasScrolling">XML Store
+      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasWithBothOptions"
+              aria-controls="offcanvasScrolling" @click = "getXmlStores">XML Store
       </button>
 
     </div>
   </nav>
 
 
-
-
-  <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+       aria-labelledby="offcanvasWithBothOptionsLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">XML Stores</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <p>Try scrolling the rest of the page to see this option in action.</p>
+      <ul class="list-group ">
+        <li v-for="item in xmlStores" :key="item.id" class="list-group-item">
+            {{item}}
+        </li>
+
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'sidebar',
-
+  data() {
+    return {
+      xmlStores: []
+    }
+  },
+  methods: {
+    getXmlStores(){
+      axios.get("/api/database/sidebar/getInfo")
+          .then((response)=>{
+            this.xmlStores = response.data.xmlStores;
+          })
+          .catch(error => {
+            console.error("Error fetching data:", error);
+            // Handle the error if needed
+          });
+    }
+  }
 }
 </script>
---
+++ src/main/java/com/example/mydatabasece/controller/sidebarController.java
@@ -0,0 +1,46 @@
+package com.example.mydatabasece.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/database/sidebar")
+public class sidebarController {
+
+    @GetMapping("/getInfo")
+    ResponseEntity<sidebarInfo> getSidebarInfo(){
+
+        sidebarInfo info = new sidebarInfo();
+
+        info.xmlStores  = new ArrayList<>();
+
+        String folderPath = "C:\\Users\\victo\\IdeaProjects\\MyDataBaseCE\\src\\main" +
+                "\\java\\com\\example\\mydatabasece\\E"; // Replace with the path of the folder you want to traverse
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+
+        assert files != null;
+
+        for (File file : files) {
+            System.out.println(file.getName());
+            info.xmlStores.add(file.getName());
+        }
+        return new ResponseEntity<sidebarInfo> (info, HttpStatus.OK);
+    }
+    public static class sidebarInfo {
+        private List<String> xmlStores;
+        public List<String> getXmlStores() {
+            return xmlStores;
+        }
+
+
+    }
+}
